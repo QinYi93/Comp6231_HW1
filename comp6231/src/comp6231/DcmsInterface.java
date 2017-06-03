bc1fc7d6746db4d05a10c60182cd16b32b69e570
QinYi93 <y_qin201@encs.concordia.ca>
2017-06-03 10:14:09 -0400

    update

---
+++ comp6231/src/comp6231/DcmsInterface.java
@@ -0,0 +1,22 @@
+package comp6231;
+
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Date;
+
+/**
+ * This class
+ *
+ * @author Yi Qin
+ * @date ${date}
+ */
+public interface DcmsInterface extends Remote{
+    public String createTRecord(String firstName, String lastName, String address,
+                                String phone, PublicParameters.Specialization specialization, PublicParameters.Location location) throws RemoteException, IOException;
+    public String createSRecord(String firstName, String lastName, PublicParameters.CoursesRegistered coursesRegistered,
+                                PublicParameters.Status status, Date statusDate) throws IOException, RemoteException;
+    public String getRecordCounts() throws IOException, RemoteException;
+    public String editRecord(String recordID, String fieldName, String newValue) throws IOException, RemoteException;
+
+}
---
+++ comp6231/src/comp6231/TeacherRecord.java
@@ -0,0 +1,73 @@
+package comp6231;
+
+/**
+ * This class
+ *
+ * @author Yi Qin
+ * @date 2017-05-28
+ */
+public class TeacherRecord extends Record {
+
+    private String address;
+    private String phone;
+    private PublicParameters.Specialization specialization;
+    private PublicParameters.Location location;
+
+    public TeacherRecord(String firstName, String lastName) {
+        super(firstName, lastName);
+        this.recordId = "TR" + baseID++;
+    }
+
+    public TeacherRecord(String firstName, String lastName, String address, String phone, PublicParameters.Specialization specialization, PublicParameters.Location location) {
+        super(firstName, lastName);
+        this.address = address;
+        this.phone = phone;
+        this.specialization = specialization;
+        this.location = location;
+        this.recordId = "TR" + baseID++;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public PublicParameters.Specialization getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(PublicParameters.Specialization specialization) {
+        this.specialization = specialization;
+    }
+
+    public PublicParameters.Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(PublicParameters.Location location) {
+        this.location = location;
+    }
+
+    public void setLocation(String location){
+        this.location = PublicParameters.Location.valueOf(location);
+    }
+    @Override
+    public String getRecordID (){
+        return this.recordId;
+    }
+
+    public void setRecordID(String recordID){
+        this.recordId = recordID;
+    }
+}
