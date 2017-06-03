c39316b0ce92a4f18bd5297c282abf38787bf307
y_qin201 <y_qin201@POLAND.ENCS.concordia.ca>
2017-06-03 12:06:36 -0400

    update

---
+++ src/assisgnment1/PublicParameters.java
@@ -0,0 +1,35 @@
+package assisgnment1;
+
+/**
+ * This class
+ *
+ * @author Yi Qin
+ * @date ${date}
+ */
+public interface PublicParameters {
+
+    enum Location{
+        MTL(SERVER_PORT_MTL),
+        LVL(SERVER_PORT_LVL),
+        DDO(SERVER_PORT_DDO);
+
+        private int port;
+
+        Location(int port) {
+            this.port = port;
+        }
+
+        public int getPort() {
+            return port;
+        }
+    };
+
+    enum Specialization {FRENCH, MATHS, SCIENCE};
+    enum CoursesRegistered{MATHS, FRENCH, SCIENCE};
+    enum Status {ACTIVE, INACTIVE};
+
+    final int SERVER_PORT_MTL = 7000;
+    final int SERVER_PORT_LVL = 7001;
+    final int SERVER_PORT_DDO = 7002;
+
+}
